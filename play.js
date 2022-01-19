b36843d694061879a87b725a71da48f715589a96
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-01-19 16:35:13 -0500

    input module create

--- play.js
+++ play.js
@@ -1,30 +1,32 @@
 
-const connect = require('./client');
-// const setupInput = require('./client')
+// const connect = require('./client');
+
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // // setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
 
-  return stdin;
-};
+//   return stdin;
+// };
 
-const handleUserInput = function(data) {
-  // your code here
-  // stdin.on('data', (key) => {
-  // \u0003 maps to ctrl+c input
-  // if (key === '\u0003') {
-  //   process.exit();
-  // }
-  // })
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function(data) {
+//   // your code here
+//   // stdin.on('data', (key) => {
+//   // \u0003 maps to ctrl+c input
+//   // if (key === '\u0003') {
+//   //   process.exit();
+//   // }
+//   // })
+//   if (data === '\u0003') {
+//     process.exit();
+//   }
+// };
 
 
 console.log("Connecting ...");
