349c7ed005555dda4110a74e0718fbedd3bfcd0b
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-01-19 14:30:37 -0500

    add client.js

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const net = require("net");
 
-// const connect = require("./client");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    // code that log out data when player get it from the server.
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
-
-// const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
