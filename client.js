24e7d4300e26fb725063b330e8078f723b06ea28
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-01-19 14:26:53 -0500

    connect to the server

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    // code that log out data when player get it from the server.
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
